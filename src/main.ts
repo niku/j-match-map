2dbb7c07757c72a5dde43c18bd88915a2faa0991
niku <10890+niku@users.noreply.github.com>
2025-02-02 14:02:33 +0900

    Use duckDB to query data

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,133 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as duckdb from "@duckdb/duckdb-wasm";
+import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url";
+import mvp_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url";
+import duckdb_wasm_eh from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url";
+import eh_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+  mvp: {
+    mainModule: duckdb_wasm,
+    mainWorker: mvp_worker,
+  },
+  eh: {
+    mainModule: duckdb_wasm_eh,
+    mainWorker: eh_worker,
+  },
+};
+// Select a bundle based on browser checks
+const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+// Instantiate the asynchronus version of DuckDB-wasm
+const worker = new Worker(bundle.mainWorker!);
+const logger = new duckdb.ConsoleLogger();
+const db = new duckdb.AsyncDuckDB(logger, worker);
+await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+
+const c = await db.connect();
+
+// matches
+await (async () => {
+  const fileName = "matches-2025.json";
+  const tableName = "matches";
+  const streamResponse = await fetch(fileName);
+  await db.registerFileBuffer(
+    fileName,
+    new Uint8Array(await streamResponse.arrayBuffer())
+  );
+  await c.insertJSONFromPath(fileName, { name: tableName });
+})();
+
+// venues
+await (async () => {
+  const fileName = "venues.json";
+  const tableName = "venues";
+  const streamResponse = await fetch(fileName);
+  await db.registerFileBuffer(
+    fileName,
+    new Uint8Array(await streamResponse.arrayBuffer())
+  );
+  await c.insertJSONFromPath(fileName, {
+    name: tableName,
+  });
+})();
+
+// for debugging
+declare global {
+  interface Window {
+    c: any;
+  }
+}
+window.c = c;
+
+// show table
+const matches = (
+  await c.query(
+    `
+  SELECT
+    matches.year,
+    matches.tournaments,
+    matches.section,
+    matches.date,
+    matches.kickoff,
+    matches.home,
+    matches.score,
+    matches.away,
+    matches.venue,
+    venues.longName AS venueLongName,
+    venues.lat AS latitude,
+    venues.lon AS longitude,
+    matches.attendance,
+    matches.broadcast
+  FROM matches
+  JOIN venues ON matches.venue = venues.shortName
 `
+  )
+)
+  .toArray()
+  .map((row) => row.toJSON());
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const tbody = matches.map((row) => {
+  return `
+    <tr>
+      <td>${row.year}</td>
+      <td>${row.tournaments}</td>
+      <td>${row.section}</td>
+      <td>${row.date}</td>
+      <td>${row.kickoff}</td>
+      <td>${row.home}</td>
+      <td>${row.score}</td>
+      <td>${row.away}</td>
+      <td>${row.venue}</td>
+      <td>${row.venueLongName}</td>
+      <td>${row.latitude}</td>
+      <td>${row.longitude}</td>
+      <td>${row.attendance}</td>
+      <td>${row.broadcast}</td>
+    </tr>
+  `;
+});
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>year</th>
+          <th>tournaments</th>
+          <th>section</th>
+          <th>date</th>
+          <th>kickoff</th>
+          <th>home</th>
+          <th>score</th>
+          <th>away</th>
+          <th>venue</th>
+          <th>venueLongName</th>
+          <th>latitude</th>
+          <th>longitude</th>
+          <th>attendance</th>
+          <th>broadcast</th>
+        </tr>
+      </thead>
+      <tbody>${tbody.join("")}</tbody>
+    </table>
+  </div>
+`;
